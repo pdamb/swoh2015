17bc385bbc4319f3e4bff386585f1d02331e17af
Petr Damborsky <pdamb@email.cz>
2015-05-12 18:20:51 +0200

    Style updates

--- app/openhouse.js
+++ app/openhouse.js
@@ -11,41 +11,11 @@ function TicketsController($http){
   
   var vm = this;
   
+  // public 
+  
   vm.headline = 'Active tickets';
   vm.selectedUser = null;
   vm.selectedTicket = 0;
-  vm.tickets = [
-    {
-      number: 1,
-      user: {
-        name: "pdamb",
-        displayName: "Petr Damborsky",
-        avatarUrl: "/images/avatars/dummy-200x200-Wardrobe-plain.jpg"
-      },
-      title: "It's broken.",
-      message: "HELP! I need a code review right now! Lorem ipsum dolor sit amet and some more radditodes ndsd snd sndsajdfn;lkjg anfdsklfj dfiorewn fkldf kjndfslnf dsifdj fdnfmsd."
-    },
-    {
-      number: 2,
-      user: {
-        name: "charlie",
-        displayName: "Charlie Chaplin",
-        avatarUrl: "/images/avatars/dummy-200x200-CharlieChaplin-plain.jpg"
-      },
-      title: "It's broken.",
-      message: "HELP! I need a code review right now! Lorem ipsum dolor sit amet and some more radditodes ndsd snd sndsajdfn;lkjg anfdsklfj dfiorewn fkldf kjndfslnf dsifdj fdnfmsd."
-    },
-    {
-      number: 3,
-      user: {
-        name: "pdamb",
-        displayName: "Petr Damborsky",
-        avatarUrl: "/images/avatars/dummy-200x200-Wardrobe-plain.jpg"
-      },
-      title: "It's broken.",
-      message: "HELP! I need a code review right now! Lorem ipsum dolor sit amet and some more radditodes ndsd snd sndsajdfn;lkjg anfdsklfj dfiorewn fkldf kjndfslnf dsifdj fdnfmsd."
-    }
-  ];
   
   this.highlight = function(ticket){
     this.selectedTicket = ticket.number;
@@ -53,8 +23,10 @@ function TicketsController($http){
     console.log(this.selectedTicket,this.selectedUser);
   };
   
+  // private
+
   $http.get('/data/tickets.json').then(function(response){
-    console.log(response.data);
     vm.tickets = response.data;
   });
+
 }
