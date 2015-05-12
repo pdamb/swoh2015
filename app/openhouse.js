c19cbaff9f959200da07834f4da738f461ee5642
Petr Damborsky <pdamb@email.cz>
2015-05-12 12:43:55 +0200

    Style and layout update

--- app/openhouse.js
+++ app/openhouse.js
@@ -8,7 +8,7 @@ app.controller('FeedController', FeedController);
 
 // Controller's Constructor Function
 function FeedController(){
-  this.headline = 'Your daily feed';
+  this.headline = 'Active tickets';
   this.feed = [
     {
       user: {
@@ -16,7 +16,31 @@ function FeedController(){
         displayName: "Petr Damborsky",
         avatarUrl: "/images/avatars/dummy-200x200-Wardrobe-plain.jpg"
       },
-      message: "HELP! I need a code review right now!"
+      message: "HELP! I need a code review right now! Lorem ipsum dolor sit amet and some more radditodes ndsd snd sndsajdfn;lkjg anfdsklfj dfiorewn fkldf kjndfslnf dsifdj fdnfmsd."
+    },
+    {
+      user: {
+        name: "pdamb",
+        displayName: "Petr Damborsky",
+        avatarUrl: "/images/avatars/dummy-200x200-Wardrobe-plain.jpg"
+      },
+      message: "HELP! I need a code review right now! Lorem ipsum dolor sit amet and some more radditodes ndsd snd sndsajdfn;lkjg anfdsklfj dfiorewn fkldf kjndfslnf dsifdj fdnfmsd."
+    },
+    {
+      user: {
+        name: "pdamb",
+        displayName: "Petr Damborsky",
+        avatarUrl: "/images/avatars/dummy-200x200-Wardrobe-plain.jpg"
+      },
+      message: "HELP! I need a code review right now! Lorem ipsum dolor sit amet and some more radditodes ndsd snd sndsajdfn;lkjg anfdsklfj dfiorewn fkldf kjndfslnf dsifdj fdnfmsd."
+    },
+    {
+      user: {
+        name: "pdamb",
+        displayName: "Petr Damborsky",
+        avatarUrl: "/images/avatars/dummy-200x200-Wardrobe-plain.jpg"
+      },
+      message: "HELP! I need a code review right now! Lorem ipsum dolor sit amet and some more radditodes ndsd snd sndsajdfn;lkjg anfdsklfj dfiorewn fkldf kjndfslnf dsifdj fdnfmsd."
     }
   ]
 }
